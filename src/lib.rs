55e3ec1f2ee31fd68fd90ece8086c77297c1946f
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2023-06-22 07:03:59 +0100

    feat: add case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    // println!("Searching for {}", contents);
-
     let lines = search(&config.query, &contents);
 
     for line in lines {
@@ -42,6 +40,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     return results;
 }
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line)
+        }
+    }
+    results
+}
 
 #[cfg(test)]
 mod test {
@@ -56,5 +65,20 @@ safe, fast, productive
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive"], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me"],
+            search_case_insensitive(query, contents)
+        )
     }
 }
